14e8e2fd8e1c9a939225a88b4756b86a06c67325
Robin Jerome <robin.jerome@outlook.com>
2016-04-07 14:18:49 +0300

    Date difference calculation logic

--- src/main/java/com/date/util/DateComparator.java
+++ src/main/java/com/date/util/DateComparator.java
@@ -1,37 +1,26 @@
 package com.date.util;
 
-import java.util.regex.Pattern;
-
 public class DateComparator {
-    private static final Pattern DATE_MATCH_PATTERN = Pattern.compile("^[0-3]\\d\\/[0-1][0-2]\\/[1-2]\\d{3}$");
-
-    public int getDateDifference(String fromDate, String toDate) throws IllegalArgumentException {
-        return 1;
-    }
-
-    private boolean validateDateString(String dateString) {
-        // Coarse date range matching (01/01/1901 - 31/12/2999)
-        if (!DATE_MATCH_PATTERN.matcher(dateString).matches()) {
-            return false;
-        }
-        // Month - day matching
-        // Leap year matching
-        return true;
-    }
 
     public static void main(String[] args) {
         DateComparator comparator = new DateComparator();
-        if (args.length == 2 && comparator.validateDateString(args[0]) && comparator.validateDateString(args[1])) {
-            System.out.println(comparator.getDateDifference(args[0], args[1]));
-            System.exit(0);
-        } else {
+        try {
+            MyDate fromDate = MyDate.makeFromString(args[0]);
+            MyDate toDate = MyDate.makeFromString(args[1]);
+            if (args.length == 2 && fromDate != null && toDate != null) {
+                System.out.println(fromDate.getFullDaysTo(toDate));
+                System.exit(0);
+            } else {
+                comparator.printUsage();
+                System.exit(1);
+            }
+        } catch (Exception e) {
             comparator.printUsage();
-            System.exit(1);
+            throw e;
         }
-
     }
 
     private void printUsage() {
-
+        System.err.println("Enter dates between (01/01/1901 - 31/12/2999) in DD/MM/YYYY format");
     }
 }
